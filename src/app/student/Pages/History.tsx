009e6802b9f5dac6addff1b4c339e8af531ca8b8
Isabel Alves <166523230+isabel-vit309@users.noreply.github.com>
2026-04-30 15:51:00 +0000

    create student portal pages and design the homepage layout

---
+++ src/app/student/Pages/History.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+
+export default function History () {
+    return (
+        <View>
+            <Text>
+                Histórico
+            </Text>
+        </View>
+
+    )
+}
